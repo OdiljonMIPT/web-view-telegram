a9f431edb4632c3796f7c2e76917bbde7e417cf8
odiljonmipt <odiljonabduvaitov@gmail.com>
2022-07-13 15:03:25 +0500

    updated WEBAPP_HOST

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ BOT_TOKEN = "5523238384:AAGh-rRIu0XmSSGdraemn6Xr1XHV5ovqixI"
 WEBHOOK_HOST = "web-view-telegram.herokuapp.com"
 WEBHOOK_PATH = "/web_app_example"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-WEBAPP_HOST = "web-view-telegram.herokuapp.com"
+WEBAPP_HOST = "https://web-view-telegram.herokuapp.com/"
 WEBAPP_PORT = 8000
